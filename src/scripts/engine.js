b1b3d4328313785dcd59f78f2d924db353143c79
Évelim Dornelles <dorrrnelles@gmail.com>
2024-09-29 18:08:13 -0300

    projeto final

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,7 +1,9 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
+
 
 let mapedKeys = [];
-
 let audio = new Audio("src/tunes/a.wav")
 
 const playTune = (key) => {
@@ -26,5 +28,16 @@ document.addEventListener("keydown", (e) => {
     if (mapedKeys.includes(e.key)) {
         playTune(e.key)
     }
-
 });
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys)
